d5088bf350fa99bb1f51b6ecb06eeb20d8ff775c
David Corticchiato <david.corticchiato@iscool-e.com>
2015-05-20 17:09:51 +0200

    generate android test project

---
+++ test/android/src/com/github/phantasien/falkor/test/FalkorTestMainActivity.java
@@ -0,0 +1,15 @@
+package com.github.phantasien.falkor.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FalkorTestMainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
