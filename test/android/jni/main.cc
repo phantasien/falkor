ef8a337e4ce25ad5b61e545c065c0ff1bab8d930
David Corticchiato <david.corticchiato@iscool-e.com>
2015-05-21 14:34:19 +0200

    execute bastian hello world on android ARM

--- test/android/jni/main.cc
+++ test/android/jni/main.cc
@@ -1,10 +1,21 @@
 #include <jni.h>
 #include <bastian.h>
+#include <android/log.h>
 
+
+BASTIAN_FUNC(Print) {
+  __android_log_print(
+    ANDROID_LOG_INFO,
+    "Falkor Test",
+    "%s",
+    func->GetArgument(0)->StringValue().c_str()
+  );
+}
 
 BASTIAN_OBJ(Global) {
+  obj->Export("print", Print);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_github_phantasien_falkor_test_FalkorTestMainActivity_run(JNIEnv* env, jobject thiz, jstring jsource) {
-  bastian::Engine::New(Global);
+  bastian::Engine::New(Global)->Run("print('Hello World !')");
 }
--- test/android/src/com/github/phantasien/falkor/test/FalkorTestMainActivity.java
+++ test/android/src/com/github/phantasien/falkor/test/FalkorTestMainActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 
 public class FalkorTestMainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    run();
+  }
+
+  public native void run();
+
+  static {
+    System.loadLibrary("falkor-test");
+  }
 }
